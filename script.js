8cb33b2e078c0884266d3e12076a72df484fc9fe
KeylaBeatriz <144954041+KeylaBeatriz@users.noreply.github.com>
2023-09-20 14:53:06 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const btnGenerate = document.querySelector("#generate-pdf");
+
+btnGenerate.addEventListener("click", () => {
+    //Conteúdo do PDF
+    const content = document.querySelector("#content");
+
+    // Configuração do arquivo final do PDF
+    const options = {
+        margin: [10, 10, 10, 10],
+        filename: "arquivo.pdf",
+        html2canvas: { scale: 2},
+        jsPDF: {unit:"mm", format: "a4", orientation: "portrait"},
+    };
+
+    //Gerar e baixar o PDF
+    html2pdf().set(options).from(content).save()
+});
